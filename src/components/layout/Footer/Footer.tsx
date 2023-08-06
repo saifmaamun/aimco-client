86b8c23aa69e719ebac3fbe1f1a9a73a7c77b3b5
Saif Maamun <sadifmamun@gmail.com>
2023-08-06 15:45:30 +0600

    header, footer

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,44 +1,62 @@
-import React from "react";
+import fb from "../../../assets/icons/facebook.png";
+import tw from "../../../assets/icons/twitter.png";
+import ln from "../../../assets/icons/linkedin.png";
+import yt from "../../../assets/icons/youtube.png";
+import ns from "../../../assets/icons/instagram.png";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer className="footer p-10 flex-auto bg-base-200 text-base-content">
-      <div className="">
+    <footer className="footer p-10 text-xs lg:text-base font-light  md:flex-auto   bg-base-200 text-base-content">
+      <div>
         <p className="footer-title border-b-2">Site information</p>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
+        <a className="link link-hover">COOKIE NOTICES</a>
+        <a className="link link-hover">PRIVACY STATEMENT</a>
+        <a className="link link-hover">TERMS AND CONDITIONS</a>
+        <a className="link link-hover">SCAM AND FRAUD ALERT</a>
+        <br />
+
+        <a className="link link-hover">SITEMAP</a>
       </div>
+
       <div>
         <span className="footer-title border-b-2">News & Media</span>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
+        <a className="link link-hover">SEARCH ALL NEWS</a>
+        <a className="link link-hover">LATEST NEWS</a>
+        <a className="link link-hover">PUBLICATIONS</a>
+        <a className="link link-hover">SPEECHES</a>
+        <a className="link link-hover">MEDIA GALLERY</a>
+        <br />
+        <a className="link link-hover">CONTACTS AND RESOURCES </a>
       </div>
+
       <div>
         <span className="footer-title border-b-2">Other websites</span>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
+        <a className="link link-hover">WA’ED VENTURES</a>
+        <a className="link link-hover">AEIMCO TRADING COMPANY</a>
+        <a className="link link-hover">IKTVA</a>
+        <a className="link link-hover">AEIMCO VENTURES</a>
+        <a className="link link-hover">CITIZENSHIP</a>
       </div>
+
       <div>
         <span className="footer-title border-b-2">Social media</span>
-        <div className="form-control w-80">
-          <label className="label">
-            <span className="label-text">Enter your email address</span>
-          </label>
-          <div className="relative">
-            <input
-              type="text"
-              placeholder="username@site.com"
-              className="input input-bordered w-full pr-16"
-            />
-            <button className="btn btn-primary absolute top-0 right-0 rounded-l-none">
-              Subscribe
-            </button>
-          </div>
+        <div className="flex gap-5 justify-center justify-items-center">
+          <Link to="/">
+            <img className="" src={fb} alt="" />
+          </Link>
+          <Link to="/">
+            <img className="" src={tw} alt="" />
+          </Link>
+          <Link to="/">
+            <img className="" src={ln} alt="" />
+          </Link>
+          <Link to="/">
+            <img className="" src={yt} alt="" />
+          </Link>
+          <Link to="/">
+            <img className="" src={ns} alt="" />
+          </Link>
         </div>
       </div>
     </footer>
--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className="navbar mt-5 bg-white">
+    <div className="navbar  font-light mt-5 md:text-sm bg-white">
       <div className="">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -24,42 +24,66 @@ export default function Navbar() {
           </label>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-white rounded-box w-52"
           >
             <li>
-              <a>Item 1</a>
+              <Link className="" to="/">
+                Who we are
+              </Link>
             </li>
             <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
+              <Link className="" to="/">
+                What we do
+              </Link>
             </li>
             <li>
-              <a>Item 3</a>
+              <Link className="" to="/">
+                Sustainability
+              </Link>
+            </li>
+            <li>
+              <Link className="" to="/">
+                Partnering with us
+              </Link>
+            </li>
+            <li>
+              <Link className="" to="/about">
+                About Us
+              </Link>
+            </li>
+            <li>
+              <Link className="" to="/contact">
+                Conact Us
+              </Link>
             </li>
           </ul>
         </div>
-        <button className="btn ms-20 me-10 rounded-none">
+        <button className="btn ms-20 me-10 hidden md:block md:ms-0 rounded-none">
           <img src={search}></img>
         </button>
       </div>
-      <div className="navbar hidden gap-9  lg:flex">
-        <Link to="/"> Who we are</Link>
-        <Link to="/"> What we do</Link>
-        <Link to="/"> Sustainability</Link>
-        <Link to="/"> Partnering with us</Link>
-        <Link to="/"> Investors</Link>
-        <Link to="/"> Careers</Link>
-        <Link to="/about"> About</Link>
-        <Link to="/contact">Contact</Link>
+      <div className="navbar hidden gap-9 lg:gap-7  font-light md:text-sm   lg:flex">
+        <Link className="" to="/">
+          Who we are
+        </Link>
+        <Link className="" to="/">
+          What we do
+        </Link>
+        <Link className="" to="/">
+          Sustainability
+        </Link>
+        <Link className="" to="/">
+          Partnering with us
+        </Link>
+
+        <Link className="" to="/about">
+          About Us
+        </Link>
+        <Link className="" to="/contact">
+          Contact Us
+        </Link>
       </div>
-      <div className="navbar-end me-9">
+      <div className="navbar-end  me-9 ms-auto me-9">
         <Link to="/">Aimco</Link>
       </div>
     </div>
